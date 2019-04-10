ce851d99cffd1c926e604f5b8ee71ad5b73c4155
Zachary Resnick <zachary.l.resnick@gmail.com>
2019-04-10 13:48:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+
+
+function theBeatlesPlay(musicians, instruments){
+  
+  let newArr = [];  //create a new array to store our results
+  
+  for (let i = 0; i < musicians.length; i++){
+    let currentMusician = musicians[i];
+    let currentInstrument = instruments[i];
+    let curString = `${currentMusician} plays ${currentInstrument}1`;
+    newArr.push(curString);
+  }
+}
+
